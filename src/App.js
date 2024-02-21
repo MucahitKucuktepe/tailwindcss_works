8be21feee79d6d4549dfe36b7ef545d9bf65bca6
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-02-21 10:30:27 +0300

    typograpy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import "./App.css";
+import Typography from "./components/Typography";
 
 function App() {
   return (
     <div>
-     
+      <h1 className="text-2xl text-center text-purple-800 font-bold">
+        Merhaba Tailwindcss
+      </h1>
+      <Typography />
     </div>
   );
 }
---
+++ src/components/Typography.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Typography = () => {
+  return (
+    <div>
+        <h3 className=' text-2xl'>Typograpy</h3>
+        <p className='text-sm'>Typography</p>
+        <p className='text-base/10'>Typography</p>
+        <p className='text-lg'>Typography</p>
+        <p className='text-xl'>Typography</p>
+        <p className='text-3xl'>Typography</p>
+        <p className='text-5xl'>Typography</p>
+        <h1 className='text-[60px]'>Baslik</h1>
+    </div>
+  )
+}
+
+export default Typography
